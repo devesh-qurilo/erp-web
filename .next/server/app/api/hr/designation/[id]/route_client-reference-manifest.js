b147b38640feea6aedd03cb1662fcfcf4c123e75
nanqurilo-lang <nanqurilo@gmail.com>
2026-01-06 11:52:12 +0530

    remove .next

--- .next/server/app/api/hr/designation/[id]/route_client-reference-manifest.js
+++
@@ -1,2 +0,0 @@
-globalThis.__RSC_MANIFEST = globalThis.__RSC_MANIFEST || {};
-globalThis.__RSC_MANIFEST["/api/hr/designation/[id]/route"] = {"moduleLoading":{"prefix":"","crossOrigin":null},"clientModules":{},"ssrModuleMapping":{},"edgeSSRModuleMapping":{},"rscModuleMapping":{},"edgeRscModuleMapping":{},"entryCSSFiles":{},"entryJSFiles":{}}
